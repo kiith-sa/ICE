9b11be24f69f772253c1395e39373fb2c1299c6c
Ferdinand Majerech <kiithsacmp@gmail.com>
2011-02-12 22:39:16 +0100

    Added license headers and license text file. Using Boost Software License 1.0 .

--- math/ssevector4.d
+++ math/ssevector4.d
@@ -1,3 +1,9 @@
+
+//          Copyright Ferdinand Majerech 2010 - 2011.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 module math.ssevector4;
 
 
@@ -5,7 +11,7 @@ import math.vector2;
 
 
 /*
- * An uniton used to prepare vector data for SSE usage.
+ * An union used to prepare vector data for SSE usage.
  * Can be used either as a 4D float vector or a pair of 2D float vectors.
  */
 align(1) union SSEVector4f
