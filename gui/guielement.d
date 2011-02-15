7d586b49be00ab7a8e3d7f54e3d1055e8025dd9b
Ferdinand Majerech <kiithsacmp@gmail.com>
2011-02-15 01:18:35 +0100

    Documentation overhaul and minor fixes: gui.guielement .

--- gui/guielement.d
+++ gui/guielement.d
@@ -30,40 +30,34 @@ class GUIElement
     protected:
         alias std.string.toString to_string;
 
+        ///Parent element of this element.
         GUIElement parent_ = null;
+        ///Children elements of this element.
         GUIElement[] children_;
 
-        //Color of the element's border.
+        ///Color of the element's border.
         Color border_color_ = Color(255, 255, 255, 96);
-        //Bounds of this element in screen space.
+        ///Bounds of this element in screen space.
         Rectanglei bounds_;
 
-        //Is this element visible?
+        ///Is this element visible?
         bool visible_ = true;
-        //Draw border of this element?
+        ///Draw border of this element?
         bool draw_border_;
-        //Are the contents of this element aligned based on its current dimensions?
+        ///Are the contents of this element aligned based on its current properties?
         bool aligned_ = false;
 
-        //Math expression used to calculate X position of the element.
+        ///Math expression used to calculate X position of the element.
         string x_string_;
-        //Math expression used to calculate Y position of the element.
+        ///Math expression used to calculate Y position of the element.
         string y_string_;
-        //Math expression used to calculate width of the element.
+        ///Math expression used to calculate width of the element.
         string width_string_;
-        //Math expression used to calculate height of the element.
+        ///Math expression used to calculate height of the element.
         string height_string_;
 
     public:
-        ~this()
-        {
-            assert(parent_ is null && children_ is null,
-                   "GUIElement must be cleared before it is destroyed");
-        }
-        
-        //This is probably not even needed, as the GC shoudln't need everything
-        //to be disconnected.
-        ///Destroy this element.
+        ///Destroy this element and all its children.
         void die()
         {
             foreach(ref child; children_)
@@ -81,18 +75,26 @@ class GUIElement
         ///Get position relative to parent element.
         final Vector2i position_local(){return bounds_.min - parent_.bounds_.min;}
         
-        ///Return size of this element in screen space.
-        final Vector2u size(){return Vector2u(bounds_.size.x, bounds_.size.y);}
+        ///Get size of this element in screen space.
+        final Vector2u size(){return to!(uint)(bounds_.size);}
 
-        ///Return bounding rectangle of this GUI element in screen space.
+        ///Get bounding rectangle of this GUI element in screen space.
         final Rectanglei bounds_global(){return bounds_;}
 
-        ///Add a child element.
+        /**
+         * Add a child element. 
+         *
+         * A single child can't be added twice to the same element or to
+         * two different GUI elements at the same time.
+         *
+         * Params:  child = Child to add.
+         */
         final void add_child(GUIElement child)
         in
         {
             assert(!children_.contains(child, true), 
                    "Trying to add a child that is already a child of this GUI element.");
+            assert(child.parent_ is null, "Trying to add a child that already has a parent");
         }
         body
         {
@@ -100,7 +102,7 @@ class GUIElement
             child.parent_ = this;
         }
 
-        ///Remove a child element.
+        ///Remove a child element. The specified element must be a child of this element.
         final void remove_child(GUIElement child)
         in
         {
@@ -113,45 +115,46 @@ class GUIElement
             child.parent_ = null;
         }
 
-        ///Return true if this element is visible, false if hidden.
+        ///Is this element visible?
         final bool visible(){return visible_;}
 
         ///Hide this element and its children.
         final void hide(){visible_ = false;}
 
-        ///Make this element and its children visible;
+        ///Make this element and its children visible.
         final void show(){visible_ = true;}
 
-        ///Return parent of this element.
-        final GUIElement parent(){return parent_;}
-
-        ///Determine if an element is a child of this element.
-        final bool is_child(GUIElement element){return parent_ is element;}
-
-    package:
+    protected:
+        /**
+         * Draw children of this element.
+         *
+         * Params:  driver = VideoDriver to draw with.
+         */
         final void draw_children(VideoDriver driver)
         {
             foreach(ref child; children_)
             {
+                //children can be null if we've just destroyed this element.
                 if(child is null){continue;}
                 child.draw(driver);
             }
         }
 
+        ///Update children of this element.
         final void update_children()
         {
             foreach(ref child; children_)
             {
+                //children can be null if we've just destroyed this element.
                 if(child is null){continue;}
                 child.update();
             }
         }
 
-    protected:
         /**
          * Construct a GUIElement with specified parameters.
          *
-         * Params:  params = Parameters for construction of GUIElement.
+         * Params:  params = Parameters for construction of the GUIElement.
          */
         this(GUIElementParams params)
         {
@@ -166,6 +169,18 @@ class GUIElement
             aligned_ = false;
         }
 
+        ///Destructor. Used to assert that the element was correctly destroyed using die().
+        ~this()
+        {
+            assert(parent_ is null && children_ is null,
+                   "GUIElement must be cleared before it is destroyed");
+        }
+        
+        /**
+         * Draw this GUIElement and its children, if visible.
+         *
+         * Params:  driver = Driver to draw with.
+         */
         void draw(VideoDriver driver)
         {
             if(!visible_){return;}
@@ -174,17 +189,23 @@ class GUIElement
 
             if(draw_border_)
             {
-                Vector2f min = Vector2f(bounds_.min.x, bounds_.min.y);
-                Vector2f max = Vector2f(bounds_.max.x, bounds_.max.y);
-                driver.draw_rectangle(min, max, border_color_);
+                driver.draw_rectangle(to!(float)(bounds_.min), to!(float)(bounds_.max), 
+                                      border_color_);
             }
 
             draw_children(driver);
         }
 
+        ///Update this GUIElement and its children.
         void update(){update_children();}
 
-        //Process keyboard input.
+        /**
+         * Process keyboard input.
+         *
+         * Params:  state   = State of the key.
+         *          key     = Keyboard key.
+         *          unicode = Unicode value of the key.
+         */
         void key(KeyState state, Key key, dchar unicode)
         {
             //ignore for hidden elements
@@ -193,12 +214,19 @@ class GUIElement
             //pass input to the children
             foreach_reverse(ref child; children_)
             {
+                //children can be null if we've just destroyed this element.
                 if(child is null){continue;}
                 child.key(state, key, unicode);
             }
         }
 
-        //Process mouse key presses. 
+        /**
+         * Process mouse key input.
+         *
+         * Params:  state    = State of the key.
+         *          key      = Mouse key.
+         *          position = Position of the mouse.
+         */
         void mouse_key(KeyState state, MouseKey key, Vector2u position)
         {
             //ignore hidden elements
@@ -207,12 +235,18 @@ class GUIElement
             //pass input to the children
             foreach_reverse(ref child; children_)
             {
+                //children can be null if we've just destroyed this element.
                 if(child is null){continue;}
                 child.mouse_key(state, key, position);
             }
         }
 
-        //Process mouse movement.
+        /**
+         * Process mouse movement.
+         *
+         * Params:  position = Position of the mouse in screen coordinates.
+         *          relative = Relative movement of the mouse.
+         */
         void mouse_move(Vector2u position, Vector2i relative)
         {
             //ignore hidden elements
@@ -221,16 +255,19 @@ class GUIElement
             //pass input to the children
             foreach_reverse(ref child; children_)
             {
+                //children can be null if we've just destroyed this element.
                 if(child is null){continue;}
                 child.mouse_move(position, relative);
             }
         }
 
-        //Realign contents of this element according to its dimensions.
+        ///Realign contents of this element according to its dimensions.
         void realign(VideoDriver driver)
         {
+            //process expressions for bounds coordinates.
             int[string] substitutions;
 
+            //substitutions for window and parents' coordinates.
             substitutions["w_right"] = driver.screen_width;
             substitutions["w_bottom"] = driver.screen_height;
             substitutions["p_left"] = parent_ is null ? 0 : parent_.bounds_.min.x;
@@ -254,10 +291,8 @@ class GUIElement
 
             bounds_.max = bounds_.min + Vector2i(width, height);
 
-            foreach(ref child; children_)
-            {
-                child.realign(driver);
-            }
+            //realign children
+            foreach(ref child; children_){child.realign(driver);}
 
             aligned_ = true;
         }
@@ -268,15 +303,22 @@ final class GUIRoot
 {
     mixin WeakSingleton;
     private:
-        //The actual GUI root element.
+        ///The actual GUI root element.
         GUIElement root_;
 
     public:
-        ///Construct the GUI root with size equal to screen size.
+        /**
+         * Construct the GUI root.
+         *
+         * Size of the root GUI element will be identical to window size.
+         *
+         * Params:  platform = Platform to use for input.
+         */
         this(Platform platform)
         {
             singleton_ctor();
 
+            //construct the root element.
             with(new GUIElementFactory)
             {
                 x = "0";
@@ -285,7 +327,6 @@ final class GUIRoot
                 height = "w_bottom";
                 root_ = produce();
             }
-
             root_.draw_border_ = false;
 
             platform.key.connect(&root_.key);
@@ -293,7 +334,11 @@ final class GUIRoot
             platform.mouse_key.connect(&root_.mouse_key);
         }
 
-        ///Draw the GUI.
+        /**
+         * Draw the GUI.
+         *
+         * Params:  driver = VideoDriver to draw with.
+         */
         void draw(VideoDriver driver)
         {
             //save view zoom and offset
@@ -312,7 +357,7 @@ final class GUIRoot
             driver.view_offset = offset;
         }
 
-        ///Get the actual root element of the GUI.
+        ///Get the root element of the GUI.
         GUIElement root(){return root_;}
 
         ///Update the GUI.
@@ -324,7 +369,7 @@ final class GUIRoot
         ///Remove a child element.
         void remove_child(GUIElement child){root_.remove_child(child);}
 
-        ///Destroy this GUIRoot.
+        ///Destroy the GUIRoot.
         void die()
         {
             root_.die();
@@ -342,42 +387,67 @@ final class GUIRoot
  */
 final class GUIElementFactory : GUIElementFactoryBase!(GUIElement)
 {
-    public GUIElement produce()
-    {
-        return new GUIElement(gui_element_params);
-    }
+    public GUIElement produce(){return new GUIElement(gui_element_params);}
 }
 
 /**
- * Template base class for all GUI element factories, template input
+ * Template base class for all GUI element factories, template type T
  * specifies type of GUI element constructed by the factory.
  *
  * Position and size of GUI elements are specified with
  * strings containing simple math expressions which
  * are evaluated to determine the actual coordinates.
  *
- * Supported operators are + +,-,*,/ as well as parentheses.
+ * Supported operators are +,-,*,/ as well as parentheses.
  *
  * Furthermore, there are builtin macros representing window and parent
  * coordinates. These are:
  *
- * w_right  : Window right end (left end is always 0, so this is window width)
- * w_bottom : Window bottom end (top end is always 0, so this is window height)
- * p_left   : Parent left end
- * p_right  : Parent right end
- * p_top    : Parent top end
- * p_bottom : Parent bottom end
+ * w_right  : Window right end (left end is always 0, so this is window width)$(BR)
+ * w_bottom : Window bottom end (top end is always 0, so this is window height)$(BR)
+ * p_left   : Parent left end$(BR)
+ * p_right  : Parent right end$(BR)
+ * p_top    : Parent top end$(BR)
+ * p_bottom : Parent bottom end$(BR)
  *
  * Params:  x           = X position math expression.
- *                        Default: "p_left"
+ *                        Default; "p_left"
  *          y           = Y position math expression. 
- *                        Default: "p_top"
+ *                        Default; "p_top"
  *          width       = Width math expression. 
- *                        Default: "64"
+ *                        Default; "64"
  *          height      = Height math expression. 
- *                        Default: "64"
+ *                        Default; "64"
  *          draw_border = Draw border of the element?
- *                        Default: true
+ *                        Default; true
+ * Examples:
+ * Example of usage of GUIElementFactory, which is used
+ * to construct instances of GUIElement, but the principle is
+ * the same for every GUI element class factory.
+ *
+ * --------------------
+ * //Assuming other is some GUI element we already have or root GUI element
+ *
+ * //construction of a GUI element:
+ *
+ * GUIElement element;
+ *
+ * with(new GUIElementFactory)
+ * {
+ *     x = "p_left + 96";
+ *     y = "p_top + 16";
+ *     width = "p_right - 192";
+ *     height = "p_bottom - 32";
+ *     element = produce();
+ *     other.add_child(element);
+ * }
+ *
+ *
+ * //destruction:
+ * other.remove_child(element);
+ * element.die();
+ * //(alternatively, we could just destroy other, which would also destroy element)
+ * --------------------
  */
 abstract class GUIElementFactoryBase(T)
 {
@@ -387,7 +457,7 @@ abstract class GUIElementFactoryBase(T)
                            "string $ height $ \"64\"",
                            "bool $ draw_border $ true"));
 
-    ///Return a struct containing factory parameters packaged for GUIElement ctor
+    ///Return a struct containing factory parameters packaged for GUIElement ctor.
     protected final GUIElementParams gui_element_params()
     {
         return GUIElementParams(x_, y_, width_, height_, draw_border_);
@@ -395,12 +465,14 @@ abstract class GUIElementFactoryBase(T)
 
     ///Return a new instance of the class produced by the factory with parameters of the factory.
     public T produce();
-
 }
 
 ///GUI element constructor parameters
 align(1) struct GUIElementParams
 {
-    string x, y, width, height;
-    bool draw_border;
+    private:
+        ///Coordinates' math expressions.
+        string x, y, width, height;
+        ///Draw border of the GUI element?
+        bool draw_border;
 }
