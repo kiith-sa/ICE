175fcad9d9b793d26a727191079d6e178e478bc9
Ferdinand Majerech <kiithsacmp@gmail.com>
2011-02-23 16:37:17 +0100

    Documentation overhaul and minor fixes: video.nodepacker .

--- video/nodepacker.d
+++ video/nodepacker.d
@@ -15,99 +15,111 @@ import math.rectangle;
 import memory.memory;
 
 
-///Binary tree based texture packer. Handles allocation of texture page space.
+/**
+ * Binary tree based texture packer. Handles allocation of texture page space.
+ *
+ * To fit a texture, space is subdivided vertically/horizontally to create an area 
+ * fitting texture width/height, try to fit the texture to that area, if it doesn't
+ * fit, subdivide again, repeat.
+ */
 package align(1) struct NodePacker
 {
     alias std.string.toString to_string;
     private:
-        //Single packer node.
+        ///Node representing a rectangular area of space.
         static align(1) struct Node
         {
             public:
-                //Area belonging to the node.
-                Rectangleu rectangle;
+                ///Area belonging to the node.
+                Rectangleu area;
             private:
-                //Children nodes.
+                ///First child.
                 Node* child_a_;
+                ///Second child.
                 Node* child_b_;
-                //True if this node's area is taken by a texture.
+                ///Is this node's area taken by a texture?
                 bool full_ = false;
 
             public:
-                ///Construct a Node with specified rectangle.
-                static Node opCall(ref Rectangleu rectangle)
+                /**
+                 * Construct a Node representing specified area.
+                 *
+                 * Params:  area = Area of the node.
+                 *
+                 * Returns: Node with specified area.
+                 */
+                static Node opCall(ref Rectangleu area)
                 {
                     Node result;
-                    result.rectangle = rectangle;
+                    result.area = area;
                     return result;
                 }
 
-                ///Try to insert a texture with given size to this node.
                 /**
-                 * @param size Size of the space needed.
+                 * Try to insert a texture with given size to this node.
                  *
-                 * @return node with space for the texture on success.
-                 * @return null on failure.
-                */
+                 * Params:  size = Size of space needed.
+                 *
+                 * Returns: Node with space for the texture on success, null on failure.
+                 */
                 Node* insert(Vector2u size)
-                in
-                {
-                    assert(size != Vector2u(0, 0), "Can't pack a zero sized texture");
-                }
+                in{assert(size != Vector2u(0, 0), "Can't pack a zero sized texture");}
                 body
                 {
                     //if not a leaf
                     if(child_a_ !is null && child_b_ !is null)
                     {
-                        //try inserting to first child
+                        //try inserting to the first child
                         Node* new_node = child_a_.insert(size);
                         if(new_node !is null){return new_node;}
                         //no room, try the second 
-                        //(which will return NULL if no room there either)
+                        //(which will return null if no room there either)
                         return child_b_.insert(size);
                     }
                     if(full_){return null;}
 
-                    Vector2u rect_size = rectangle.size;
+                    Vector2u area_size = area.size;
                     //if this node is too small
-                    if(rect_size.x < size.x || rect_size.y < size.y){return null;}
+                    if(area_size.x < size.x || area_size.y < size.y){return null;}
                     //if exact fit
-                    if(rect_size == size)
+                    if(area_size == size)
                     {
                         full_ = true;
                         return this;
                     }
-                    child_a_ = alloc_struct!(Node)(rectangle);
-                    child_b_ = alloc_struct!(Node)(rectangle);
+                    child_a_ = alloc_struct!(Node)(area);
+                    child_b_ = alloc_struct!(Node)(area);
 
                     //decide which way to split
-                    Vector2u free_space = rect_size - size;
+                    Vector2u free_space = area_size - size;
                     //split with a vertical cut if more free space on the right
                     if(free_space.x > free_space.y)
                     {
-                        child_a_.rectangle.max.x = rectangle.min.x + size.x;// - 1;
-                        child_b_.rectangle.min.x += size.x;
+                        child_a_.area.max.x = area.min.x + size.x;// - 1;
+                        child_b_.area.min.x += size.x;
                     }
                     //split with a horizontal cut if more free space on the bottom
                     else
                     {
-                        child_a_.rectangle.max.y = rectangle.min.y + size.y;// - 1;
-                        child_b_.rectangle.min.y += size.y;
+                        child_a_.area.max.y = area.min.y + size.y;// - 1;
+                        child_b_.area.min.y += size.y;
                     }
                     return child_a_.insert(size);
                 }
 
-                ///Try to remove a texture with specified area.
+                //could be optimized using simple rectangle intersection
+                //(probably not much gain, though).
                 /**
-                 * @return true on success.
-                 * @return false on failure.
-                 * @note could be optimized using simple rectanlge intersection
-                 * (probably not much gain, though).
+                 * Try to remove a texture with specified area.
+                 *
+                 * Params:  rect = Area of texture to remove.
+                 *
+                 * Returns: True on success, false on failure.
                  */
                 bool remove(ref Rectangleu rect)
                 {
                     //exact fit, this is the area we want to free
-                    if(rect == rectangle && full_)
+                    if(rect == area && full_)
                     {
                         full_ = false;
                         return true;
@@ -144,59 +156,69 @@ package align(1) struct NodePacker
                 }
         }
 
-        //Size of the area available to the packer, in pixels.
+        ///Size of the area available to the packer in pixels.
         Vector2u size_;
 
-        //Root node of the packer tree.
+        ///Root node of the packer tree.
         Node* root_;
 
     public:
-        ///Fake constructor. Returns NodePacker with specified texture size.
+        /**
+         * Construct NodePacker.
+         *
+         * Params:  size = Size of texture area for the packer to manage.
+         * 
+         * Returns: NodePacker with specified size.
+         */
         static NodePacker opCall(Vector2u size)
         {
             NodePacker packer;
-            packer.ctor(size);
+            packer.size_ = size;
+            packer.root_ = alloc_struct!(Node)(Rectangleu(Vector2u(0, 0), size));
             return packer;
         }
 
         ///Destroy this NodePacker and its nodes.
         void die(){free(root_);}
 
-        ///Try to allocate space for a texture with given size.
         /**
-         * @param size Size of the texture to allocate space for.
-         * @param texcoords Texture coordinates of the texture will be output here.
-         * @param offset Offset of the texture on the page will be output here.
+         * Try to allocate space for a texture with given size.
          *
-         * @return true on success.
-         * @return false on failure.
+         * Params:  size      = Size of texture space to allocate.
+         *          texcoords = Texture coords of the texture on the page will be written here.
+         *          offset    = Offset of the texture on the page will be written here.
+         *        
+         * Returns: True if successful, false otherwise.
          */
-        bool allocate_space(Vector2u size, out Rectanglef texcoords, 
-                            out Vector2u offset)
+        bool allocate_space(Vector2u size, out Rectanglef texcoords, out Vector2u offset)
         {
             Node* node = root_.insert(size);
             if(node is null){return false;}
 
-            Vector2f min = to!(float)(node.rectangle.min);
-            Vector2f max = to!(float)(node.rectangle.max);
+            Vector2f min = to!(float)(node.area.min);
+            Vector2f max = to!(float)(node.area.max);
 
             texcoords.min = Vector2f(min.x / size_.x, min.y / size_.y);
             texcoords.max = Vector2f(max.x / size_.x, max.y / size_.y);
-            offset = node.rectangle.min;
+            offset = node.area.min;
             return true;
         }
 
-        ///Free space taken by a texture.
-        void free_space(ref Rectangleu rectangle)
+        /**
+         * Free space taken by a texture.
+         * 
+         * Params:  area = Area of the texture to free.
+         */
+        void free_space(ref Rectangleu area)
         {
-            bool removed = root_.remove(rectangle);
+            bool removed = root_.remove(area);
             assert(removed, "Trying to remove unallocated space from NodePacker");
         }
 
         ///Determine if this NodePacker is empty.
         bool empty(){return root_.empty();}
 
-        ///Return a string containing information about this packer.
+        ///Return a string containing information about the packer.
         string info()
         {
             uint nodes, leaves, full, full_area;
@@ -213,7 +235,7 @@ package align(1) struct NodePacker
                     if(n.full_)
                     {
                         ++full;
-                        full_area += n.rectangle.area;
+                        full_area += n.area.area;
                     }
                 }
             }
@@ -231,14 +253,6 @@ package align(1) struct NodePacker
             output ~= "farea: " ~ to_string(full_percent) ~ "%\n";
             output ~= "earea: " ~ to_string(empty_percent) ~ "%\n";
             return output;
-        }
-
-    private:
-        ///Initialization method used by the fake constructor.
-        void ctor(Vector2u size)
-        {
-            size_ = size;
-            root_ = alloc_struct!(Node)(Rectangleu(Vector2u(0, 0), size));
         }
 }   
 
