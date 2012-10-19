a74d9dae5e8ebe13548e2ecfc6115399722edab0
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-10-19 17:50:50 +0200

    Added SlotWidget stub (a widget that can connect to a RootWidget).

---
+++ gui2/slotwidget.d
@@ -0,0 +1,50 @@
+
+//          Copyright Ferdinand Majerech 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+/// A widget acting as a pluggable slot in a widget tree.
+module gui2.slotwidget;
+
+
+import gui2.widget;
+import gui2.rootwidget;
+import util.yaml;
+
+
+/// A widget that can contain a RootWidget.
+///
+/// A SlotWidget is a slot in the widget tree where different 
+/// widget trees might be inserted.
+class SlotWidget : Widget
+{
+private:
+    /// Connected widget, if any.
+    RootWidget connectedWidget_;
+
+public:
+    /// Load a SlotWidget from YAML.
+    this(ref YAMLNode yaml)
+    {
+        assert(false, "TODO");
+        super(yaml);
+    }
+
+    /// Connect a RootWidget.
+    void connect(RootWidget child)
+    {
+        assert(connectedWidget_ is null, "There is already a connected RootWidget");
+        connectedWidget_ = child;
+        assert(false, "TODO (add child to subwidgets)");
+    }
+
+    /// Disconnect a RootWidget. The RootWidget passed must be the connected RootWidget.
+    void disconnect(RootWidget child)
+    {
+        assert(connectedWidget_ is child, "The widget to disconnect does not match");
+        connectedWidget_ = null;
+        assert(false, "TODO (remove child to subwidgets)");
+    }
+}
