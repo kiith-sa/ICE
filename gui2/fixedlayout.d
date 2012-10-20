71971648a93ac12a221f81d60d4b559221333767
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-10-20 19:40:45 +0200

    Added FixedLayout, a simple layout that never changes.

---
+++ gui2/fixedlayout.d
@@ -0,0 +1,38 @@
+
+//          Copyright Ferdinand Majerech 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+/// Fixed layout that never changes.
+module layout.fixedlayout;
+
+
+import gui2.layout;
+import gui2.widget;
+import gui2.widgetutils;
+import math.rect;
+import math.vector2;
+import util.yaml;
+
+
+/// Fixed layout - bounds of the widget are set from the start and don't change.
+///
+/// Useful for e.g. the root widget.
+class FixedLayout: Layout
+{
+public:
+    /// Construct a FixedLayout from YAML.
+    this(ref YAMLNode yaml)
+    {
+        bounds_.min = Vector2i(layoutInitProperty!(int, "x")(yaml),
+                               layoutInitProperty!(int, "y")(yaml));
+        bounds_.max = bounds_.min + 
+                      Vector2i(layoutInitProperty!(int, "w")(yaml),
+                               layoutInitProperty!(int, "h")(yaml));
+    }
+
+    override void minimize(Widget[] children){};
+    override void expand(Widget parent){};
+}
