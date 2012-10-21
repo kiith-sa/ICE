a1f3af8fa85e7ef7f68fca9edbd26d499c435a0b
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-10-21 21:06:13 +0200

    Added ContainerWidget; a widget to group/lay out other widgets.

---
+++ gui2/containerwidget.d
@@ -0,0 +1,27 @@
+
+//          Copyright Ferdinand Majerech 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+/// A simple container widget.
+module gui2.containerwidget;
+
+
+import gui2.guisystem;
+import gui2.widget;
+import util.yaml;
+
+/// A simple container widget.
+///
+/// Currently, this has no functionality - it's only used to group/lay out 
+/// child widgets.
+class ContainerWidget: Widget
+{
+    /// Construct a ContainerWidget from YAML.
+    this(ref YAMLNode yaml, GUISystem guiSystem)
+    {
+        super(yaml, guiSystem);
+    }
+}
