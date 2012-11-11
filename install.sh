41946bc205aa9fc1c2a18fcb6842d20ab1468578
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-11-11 14:27:04 +0100

    Minor install.sh fix (still doesn't work, though).

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp -r data /usr/local/share/ice
+mkdir $HOME/.ice
 cp -r ice-debug /usr/bin/ice.bin
 rm -f /usr/bin/ice
 echo -e "#!/bin/bash\nice.bin -R=/usr/local/share/ice -U=$HOME/.ice" >> /usr/bin/ice
