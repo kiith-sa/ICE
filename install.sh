9837d86f42958046197bf19c565d4523c62d4449
kiith-sa <kiith-sa@Defenestrator.(none)>
2012-12-15 15:01:31 +0100

    Updated the Linux install script.

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-cp -r data /usr/local/share/ice
-mkdir $HOME/.ice
-cp -r ice-debug /usr/bin/ice.bin
-rm -f /usr/bin/ice
-echo -e "#!/bin/bash\nice.bin -R=/usr/local/share/ice -U=$HOME/.ice" >> /usr/bin/ice
-chmod +x /usr/bin/ice
+DATA_DIR='/usr/local/share/ice-game'
+
+if [ "$(whoami)" != "root" ]; then
+    echo "This script requires root priviledges; use as root or use sudo."
+    exit 1
+fi
+
+if [ -d "$DATA_DIR" ]; then 
+    cp -r data/* $DATA_DIR
+else
+    cp -r data $DATA_DIR
+fi
+
+cp -r ice-debug /usr/bin/ice-game.bin
+rm -f /usr/bin/ice-game
+echo -e "#!/bin/bash\nice-game.bin -R=$DATA_DIR -U=$HOME/.ice-game" >> /usr/bin/ice-game
+chmod +x /usr/bin/ice-game
+
+echo 'Done.'
