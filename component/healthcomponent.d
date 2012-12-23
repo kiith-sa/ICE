d009e6c8d27ff12bcab778112050506df0e77b95
Kiith-Sa <kiith-sa@BlackCore.(none)>
2012-12-23 14:05:52 +0100

    Implemented shields.

--- component/healthcomponent.d
+++ component/healthcomponent.d
@@ -9,6 +9,8 @@
 module component.healthcomponent;
 
 
+import std.algorithm;
+
 import component.entitysystem;
 
 import math.math;
@@ -18,30 +20,72 @@ import util.yaml;
 ///Makes an entity have limited health and die when that health is reduced to zero.
 struct HealthComponent
 {
-    ///Maximum health.
+    /// Maximum health.
     uint maxHealth;
 
-    ///Current health.
+    /// Current health.
     uint health;
 
-    ///ID of the last entity we've been damaged by.
+    /// Maximum shield.
+    uint maxShield;
+
+    /// Current shield. Float for gradual reloading.
+    float shield;
+
+    /// How much the shield regenerates per second.
+    uint shieldReloadRate;
+
+    /// ID of the last entity we've been damaged by.
     EntityID mostRecentlyDamagedBy;
 
-    ///Have we've been damaged during this update?
+    /// Have we've been damaged during this update?
     bool damagedThisUpdate = false;
 
-    ///Load from a YAML node. Throws YAMLException on error.
+    /// Load from a YAML node. Throws YAMLException on error.
     this(ref YAMLNode yaml)
     {
-        health = maxHealth = yaml.as!uint;
+        if(yaml.isScalar)
+        {
+            health = maxHealth = yaml.as!uint;
+        }
+        else
+        {
+            health = maxHealth = yaml["health"].as!uint;
+            shield = maxShield = 
+                yaml.containsKey("shield") ? yaml["shield"].as!uint : 0;
+            shieldReloadRate = yaml.containsKey("shieldReloadRate") 
+                             ? yaml["shieldReloadRate"].as!uint : 50;
+        }
     }
 
-    ///Apply damage (or healing, if negative).
+    /// Apply damage (or healing, if negative).
     void applyDamage(const EntityID damagedBy, const int damage)
     {
         mostRecentlyDamagedBy = damagedBy;
         damagedThisUpdate = true;
+        if(hasShield)
+        {
+            // Healing doesn't apply to the shield.
+            if(damage < 0)
+            {
+                health = min(maxHealth, health - damage);
+                return;
+            }
+
+            const shieldInt = cast(int)shield;
+            // If this is negative, the attack damages health.
+            int shieldLeft = shieldInt - damage;
+            shield = max(shieldLeft, 0);
+            if(shieldLeft >= 0) {return;}
+            // Apply the damage shield couldn't avoid.
+            // Must cast to int to avoid underflow when adding a negative to uint.
+            health = max(0, cast(int)health + shieldLeft);
+            return;
+        }
         health = clamp(0, cast(int)health - damage, cast(int)maxHealth);
     }
+
+    /// Does the entity with this component have a shield?
+    @property bool hasShield() const pure nothrow {return maxShield > 0;}
 }
 
--- component/healthsystem.d
+++ component/healthsystem.d
@@ -5,70 +5,85 @@
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 
-///Manages entity health and destroys entities that have run out of health.
+/// Manages entity health and destroys entities that have run out of health.
 module component.healthsystem;
 
+
+import std.algorithm;
 
 import component.entitysystem;
 import component.healthcomponent;
 import component.system;
 
+import time.gametime;
+
 
 ///Manages entity health and destroys entities that have run out of health.
 class HealthSystem : System 
 {
-    private:
-        ///Entity system whose data we're processing.
-        EntitySystem entitySystem_;
+private:
+    /// Entity system whose data we're processing.
+    EntitySystem entitySystem_;
 
-    public:
-        ///Construct a HealthSystem working on entities from specified EntitySystem.
-        this(EntitySystem entitySystem)
+    /// Game time subsystem, used for any regeneration (e.g. shield reload).
+    GameTime gameTime_;
+
+public:
+    /// Construct a HealthSystem working on entities from specified EntitySystem and GameTime.
+    this(EntitySystem entitySystem, GameTime gameTime)
+    {
+        entitySystem_  = entitySystem;
+        gameTime_      = gameTime;
+    }
+
+    /// Destroy entities that have run out of health.
+    void update()
+    {
+        foreach(ref Entity e, ref HealthComponent health; entitySystem_)
         {
-            entitySystem_  = entitySystem;
+            // Reload any shields.
+            if(health.hasShield)
+            {
+                health.shield = 
+                    min(health.maxShield, 
+                        health.shield + health.shieldReloadRate * gameTime_.timeStep);
+            }
+            if(health.health == 0)
+            {
+                updateStatisticsOfKiller(health);
+
+                e.kill();
+            }
+            health.damagedThisUpdate = false;
+        }
+    }
+
+private:
+    /// Update statistics of the entity that killed the entity with specified HealthComponent.
+    void updateStatisticsOfKiller(ref HealthComponent health)
+    {
+        if(!health.damagedThisUpdate) {return;}
+
+        //Update statistics of whoever killed us.
+        EntityID id = health.mostRecentlyDamagedBy;
+        Entity* damagedBy;
+
+        //If damagedBy has an owner, get the owner, if the owner
+        //has an owner, get that, etc.
+        for(;;)
+        {
+            damagedBy = entitySystem_.entityWithID(id);
+            //The entity was destroyed.
+            if(damagedBy is null){return;}
+            auto owner = damagedBy.owner;
+            if(owner is null){break;}
+            id = owner.ownerID;
         }
 
-        ///Destroy entities that have run out of health.
-        void update()
+        auto statistics = damagedBy.statistics;
+        if(statistics !is null)
         {
-            foreach(ref Entity e, ref HealthComponent health; entitySystem_)
-            {
-                if(health.health == 0)
-                {
-                    updateStatisticsOfKiller(health);
-
-                    e.kill();
-                }
-                health.damagedThisUpdate = false;
-            }
+            ++statistics.entitiesKilled;
         }
-
-    private:
-        ///Update statistics of the entity that killed the entity with specified HealthComponent.
-        void updateStatisticsOfKiller(ref HealthComponent health)
-        {
-            if(!health.damagedThisUpdate) {return;}
-
-            //Update statistics of whoever killed us.
-            EntityID id = health.mostRecentlyDamagedBy;
-            Entity* damagedBy;
-
-            //If damagedBy has an owner, get the owner, if the owner
-            //has an owner, get that, etc.
-            for(;;)
-            {
-                damagedBy = entitySystem_.entityWithID(id);
-                //The entity was destroyed.
-                if(damagedBy is null){return;}
-                auto owner = damagedBy.owner;
-                if(owner is null){break;}
-                id = owner.ownerID;
-            }
-
-            auto statistics = damagedBy.statistics;
-            if(statistics !is null)
-            {
-                ++statistics.entitiesKilled;
-            }
-        }
+    }
 }
