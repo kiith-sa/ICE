d81b38bf7dab1f9851ea725687a0df005f62dc6c
Kiith-Sa <kiith-sa@BlackCore.(none)>
2013-02-18 13:48:11 +0100

    Minor reformatting.

--- time/eventcounter.d
+++ time/eventcounter.d
@@ -26,64 +26,64 @@ import util.signal;
  */
 struct EventCounter
 {
-    private:
-        ///Time when this eventcounter started.
-        real startTime_;
-        ///Used for periodic updates.
-        Timer period_;
+private:
+    ///Time when this eventcounter started.
+    real startTime_;
+    ///Used for periodic updates.
+    Timer period_;
 
-        ///Number of events last period.
-        uint eventsPeriod_ = 0;
-        ///Total number of events.
-        uint eventsTotal_ = 0;
-        ///Total number of events until the end of the last period.
-        uint eventsTotalLastPeriod_ = 0;
+    ///Number of events last period.
+    uint eventsPeriod_ = 0;
+    ///Total number of events.
+    uint eventsTotal_ = 0;
+    ///Total number of events until the end of the last period.
+    uint eventsTotalLastPeriod_ = 0;
 
-    public:
-        ///Emitted when a period ends - passes events per second.
-        mixin Signal!real update;
+public:
+    ///Emitted when a period ends - passes events per second.
+    mixin Signal!real update;
 
-        /**
-         * Construct an EventCounter. 
-         * 
-         * Params:  period = Update period.
-         */
-        this(const real period)
+    /**
+     * Construct an EventCounter. 
+     * 
+     * Params:  period = Update period.
+     */
+    this(const real period)
+    {
+        period_ = Timer(period);
+        startTime_ = getTime();
+    }
+
+    ///Destroy this EventCounter.
+    ~this(){update.disconnectAll();}
+
+    ///Count one event.
+    void event()
+    {
+        if(period_.expired())
         {
-            period_ = Timer(period);
-            startTime_ = getTime();
+            eventsPeriod_ = eventsTotal_ - eventsTotalLastPeriod_;
+
+            const real updatesSecond = eventsPeriod_ / period_.age();
+            update.emit(updatesSecond);
+
+            eventsTotalLastPeriod_ = eventsTotal_;
+            period_.reset();
         }
+        ++eventsTotal_;
+    }
 
-        ///Destroy this EventCounter.
-        ~this(){update.disconnectAll();}
+    ///Get number of events counted so far.
+    @property uint events() const pure {return eventsTotal_;}
 
-        ///Count one event.
-        void event()
-        {
-            if(period_.expired())
-            {
-                eventsPeriod_ = eventsTotal_ - eventsTotalLastPeriod_;
+    ///Return a string containing statistics about events counted.
+    @property string statistics() const
+    {
+        const real timeTotal = getTime() - startTime_;
+        const real eventsSecond = eventsTotal_ / timeTotal;
 
-                const real updatesSecond = eventsPeriod_ / period_.age();
-                update.emit(updatesSecond);
-
-                eventsTotalLastPeriod_ = eventsTotal_;
-                period_.reset();
-            }
-            ++eventsTotal_;
-        }
-
-        ///Get number of events counted so far.
-        @property uint events() const pure {return eventsTotal_;}
-
-        ///Return a string containing statistics about events counted.
-        @property string statistics() const
-        {
-            const real timeTotal = getTime() - startTime_;
-            const real eventsSecond = eventsTotal_ / timeTotal;
-
-            return "Total events: " ~ to!string(eventsTotal_) ~ "\n" 
-                   ~ "Total Time: " ~ to!string(timeTotal) ~ "\n" 
-                   ~ "Average events per second: " ~ to!string(eventsSecond);
-        }
+        return "Total events: " ~ to!string(eventsTotal_) ~ "\n" 
+               ~ "Total Time: " ~ to!string(timeTotal) ~ "\n" 
+               ~ "Average events per second: " ~ to!string(eventsSecond);
+    }
 }
